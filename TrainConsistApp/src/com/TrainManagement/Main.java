042bf93e94f7494d87804851d543bb057bd6d7ab
harshpateriya12 <harsh.pateriya1@gmail.com>
2026-03-10 15:37:46 -0700

    Coded UC6 for Train Bogie Capacity Using HashMap

--- TrainConsistApp/src/com/TrainManagement/Main.java
+++ TrainConsistApp/src/com/TrainManagement/Main.java
@@ -1,28 +1,58 @@
 package com.TrainManagement;
 
-import java.util.LinkedHashSet;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
 
     public static void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
 
-        LinkedHashSet<String> bogies = new LinkedHashSet<>();
+        HashMap<String, Integer> bogieCapacity = new HashMap<>();//using hashmap
 
-        System.out.println("Enter number of bogies:");
+        System.out.println("***Bogie Capacity Mapping System***");
+
+        System.out.println("Enter number of bogies to map:");
         int n = sc.nextInt();
         sc.nextLine();
 
         for(int i=0;i<n;i++)
         {
             System.out.println("Enter Bogie Name:");
-            bogies.add(sc.nextLine());
+            String bogieName = sc.nextLine();
+
+            System.out.println("Enter Capacity:");
+            int capacity = sc.nextInt();
+            sc.nextLine();
+
+            bogieCapacity.put(bogieName, capacity);//inserting values
         }
 
-        System.out.println("\nBogies in insertion order:");
-        System.out.println(bogies);
+        System.out.println("\nBogie Capacity Details:");
 
+        for(Map.Entry<String,Integer> entry : bogieCapacity.entrySet())
+        {
+            System.out.println("Bogie : " + entry.getKey() +
+                               " Capacity : " + entry.getValue());
+        }
+
+        System.out.println("\nSearch Capacity of a Bogie");
+
+        System.out.println("Enter Bogie Name:");
+        String search = sc.nextLine();
+
+        if(bogieCapacity.containsKey(search))
+        {
+            System.out.println("Capacity of " + search + " is "
+                    + bogieCapacity.get(search));
+        }
+        else
+        {
+            System.out.println("Bogie not found.");
+        }
+
+        System.out.println("\nProgram Continues...");
     }
 }
