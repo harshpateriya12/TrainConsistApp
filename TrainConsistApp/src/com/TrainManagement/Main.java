65119ab2c6ceca3eab803e67692dce298fbad3bc
harshpateriya12 <harsh.pateriya1@gmail.com>
2026-03-12 10:28:37 -0700

    Coded UC8 for filtering based on Bogie Capacity

--- TrainConsistApp/src/com/TrainManagement/Main.java
+++ TrainConsistApp/src/com/TrainManagement/Main.java
@@ -1,10 +1,11 @@
 package com.TrainManagement;
 
 import com.model.Bogie;
+
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -14,13 +15,13 @@ public class Main {
 
         List<Bogie> bogieList = new ArrayList<>();
 
-        System.out.println("*** Sort Bogies by Capacity ***");
+        System.out.println("***Filter Bogies Using Streams***");
 
         System.out.println("Enter number of bogies:");
         int n = sc.nextInt();
         sc.nextLine();
 
-        for (int i = 0; i < n; i++) {
+        for(int i = 0; i < n; i++) {
 
             System.out.println("Enter Bogie Name:");
             String name = sc.nextLine();
@@ -32,19 +33,29 @@ public class Main {
             bogieList.add(new Bogie(name, capacity));
         }
 
-        System.out.println("\nBogies Before Sorting:");
-        for (Bogie b : bogieList) {
-            System.out.println(b);
+        System.out.println("\nAll Bogies:");
+        bogieList.forEach(System.out::println);
+
+        System.out.println("\nEnter minimum capacity to filter bogies:");
+        int minCapacity = sc.nextInt();
+
+        // Stream Filtering
+        List<Bogie> filteredBogies =
+                bogieList.stream()
+                        .filter(b -> b.getCapacity() > minCapacity)
+                        .collect(Collectors.toList());
+
+        System.out.println("\nFiltered Bogies (Capacity > " + minCapacity + "):");
+
+        if(filteredBogies.isEmpty())
+        {
+            System.out.println("No bogies match the condition.");
+        }
+        else
+        {
+            filteredBogies.forEach(System.out::println);
         }
 
-        // Sorting using Comparator and Lambda
-        bogieList.sort(Comparator.comparingInt(Bogie::getCapacity));
-
-        System.out.println("\nBogies Sorted by Capacity (Ascending):");
-        for (Bogie b : bogieList) {
-            System.out.println(b);
-        }
-
-        
+       
     }
 }
